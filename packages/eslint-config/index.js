0fa2e62dfd6670018266c3de3e513b50c472ad63
liubingwen <liubingwen@huianrong.com>
2021-12-09 17:56:00 +0800

    feat(eslint-config): 添加配置

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: 2020,
+    sourceType: "module",
+    extraFileExtensions: [".vue"],
+  },
+  plugins: ["@typescript-eslint"],
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
