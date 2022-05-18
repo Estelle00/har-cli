2557ebcb2e429b42592bb93e7ba71e013c44abbe
liubingwen <liubingwen@huianrong.com>
2022-05-18 13:57:08 +0800

    feature(vite-plugin-external): 添加自动屏蔽当前package文件引用

--- packages/vite-plugin-external/index.ts
+++ packages/vite-plugin-external/index.ts
@@ -1,9 +1,22 @@
 import type { Plugin } from "vite";
+import {join} from "node:path";
+import {existsSync, readFileSync, statSync} from "fs";
+function lookupPackageFile(dir = process.cwd()) {
+  const fullPath = join(dir, "package.json");
+  if (existsSync(fullPath) && statSync(fullPath).isFile()) {
+    return JSON.parse(readFileSync(fullPath, "utf-8"));
+  }
+  return false;
+}
+const data = lookupPackageFile();
 export default function externalPlugin(): Plugin {
   return {
     name: "vite:external-node_modules",
     enforce: "pre",
     async resolveId(source: string, importer) {
+      if (new RegExp("^" + data.name).test(source)) {
+        return false;
+      }
       const result = await this.resolve(source, importer, {
         skipSelf: true,
         custom: { "node-resolve": {} },
